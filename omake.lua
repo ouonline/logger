a9a417257aae569868274f90294ba37dd70ec9a8
Guoyu Ou <benogy@gmail.com>
2020-04-09 21:42:12 +0800

    Makefile changes

--- omake.lua
+++ omake.lua
@@ -1,8 +1,15 @@
 project = CreateProject()
 
-target = project:CreateLibrary("logger", STATIC | SHARED)
-target:AddSourceFiles("*.c")
-target:AddLibrary("../utils", "utils", STATIC)
-target:AddSysLibraries("pthread")
+dep = project:CreateDependency()
+dep:AddSourceFiles("*.c")
+dep:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+dep:AddStaticLibrary("../utils", "utils_static")
+dep:AddSysLibraries("pthread")
+
+a = project:CreateStaticLibrary("logger_static")
+a:AddDependencies(dep)
+
+so = project:CreateSharedLibrary("logger_shared")
+so:AddDependencies(dep)
 
 return project
--- tests/omake.lua
+++ tests/omake.lua
@@ -2,6 +2,7 @@ project = CreateProject()
 
 target = project:CreateBinary("test_logger")
 target:AddSourceFiles("*.c")
-target:AddLibrary("..", "logger", STATIC)
+target:AddFlags("-Wall", "-Werror", "-Wextra", "-fPIC")
+target:AddStaticLibrary("..", "logger_static")
 
 return project
